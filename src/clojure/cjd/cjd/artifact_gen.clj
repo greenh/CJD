259902250933b2e7184f005ffc51136c08afe968
Howard Green <hhgreen@ieee.org>
2011-12-15 21:49:34 -0800

    Adds copyright statement to source files that didn't already have one.

--- src/clojure/cjd/cjd/artifact_gen.clj
+++ src/clojure/cjd/cjd/artifact_gen.clj
@@ -1,4 +1,13 @@
-
+#_ ( Copyright (c) 2011 Howard Green. All rights reserved.
+                
+     The use and distribution terms for this software are covered by the
+     Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+     which can be found in the file epl-v10.html at the root of this distribution.
+     By using this software in any fashion, you are agreeing to be bound by
+     the terms of this license.
+     
+     You must not remove this notice, or any other, from this software.
+     )
 #_ (* Adds support for CJD-specific artifacts to the base CJD HTML generator.
       @p This is an extension to the base CDJ state of affairs, and is intended
       to be loaded by the "--requires" option (command-line version) or 
--- src/clojure/cjd/util/string_utils.clj
+++ src/clojure/cjd/util/string_utils.clj
@@ -1,3 +1,15 @@
+#_ ( Copyright (c) 2011 Howard Green. All rights reserved.
+                
+     The use and distribution terms for this software are covered by the
+     Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+     which can be found in the file epl-v10.html at the root of this distribution.
+     By using this software in any fashion, you are agreeing to be bound by
+     the terms of this license.
+     
+     You must not remove this notice, or any other, from this software.
+     )
+#_ (* Provides a set of occasionally useful string-related function.
+      )
 (ns cjd.util.string-utils)
 
 #_ (* Constructs a string (by applying @(link clojure.core/str str)) 
